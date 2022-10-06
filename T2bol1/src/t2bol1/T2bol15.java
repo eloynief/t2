79105beb0dbd5644932c7ef5a35ae4b165a6213e
enieves <enieves@106-29.info2>
2022-10-06 13:49:36 +0200

    t2

--- T2bol1/src/t2bol1/T2bol15.java
+++ T2bol1/src/t2bol1/T2bol15.java
@@ -32,33 +32,33 @@ public class T2bol15 {
 		num3 = sc.nextDouble();
 		
 		if(num3<num2 && num2<num1){
-		System.out.print(num1 + " " + num2 + " " + num3);
+		System.out.print(num1 + ", " + num2 + ", " + num3);
 		}
 		
 		//comando else if el cual hace su función si if no la hace y si es verdad lo escrito
 		else if(num2<num3 && num3<num1){
 		
-		System.out.print(num1 + " " + num3 + " " + num2);	
+		System.out.print(num1 + ", " + num3 + ", " + num2);	
 		}
 		
 		//repetición del comando else if con los otros valores si el else if no hace su función
 		//si num2<num1<num3, se activa esta función
 		else if(num2<num1 && num1<num3){
-		System.out.print(num3 + " " + num1 + " " + num2);
+		System.out.print(num3 + ", " + num1 + ", " + num2);
 		}
 		
 		//repetición del comando else if con los otros valores si los demás else if no hacen su función
 		//si num1<num2<num3, se activa esta función
 		else if(num1<num2 && num2<num3){
-		System.out.print(num3 + " " + num2 + " " + num1);
+		System.out.print(num3 + ", " + num2 + ", " + num1);
 		}
 		
 		else if(num1<num3 && num3<num2){
-		System.out.print(num2 + " " + num3 + " " + num1);
+		System.out.print(num2 + ", " + num3 + ", " + num1);
 		}
 		
 		else{
-		System.out.print(num2 + " " + num1 + " " + num3);
+		System.out.print(num2 + ", " + num1 + ", " + num3);
 		}
 		
 		//cierre de scanner
