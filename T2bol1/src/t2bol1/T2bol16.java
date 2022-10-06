88aecb04092e8c824917c236d5a2b38b87c30fac
enieves <enieves@106-29.info2>
2022-10-06 14:04:44 +0200

    t2

--- T2bol1/src/t2bol1/T2bol16.java
+++ T2bol1/src/t2bol1/T2bol16.java
@@ -1,10 +1,42 @@
 package t2bol1;
 
+import java.util.Scanner;
+
 public class T2bol16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//variable para guardar un número entero
+		double a, b, c, xsum, xres, raiz;
+				
+		//Creación de scanner
+		Scanner sc = new Scanner(System.in);
+				
+		//Le decimos al usuario que introduzca un número
+		System.out.print("Introduce el primer número: ");
+		
+		//Comando para introducir el primer número
+		a = sc.nextDouble();
+		
+		//Le decimos al usuario que introduzca un número
+		System.out.print("Introduce el segundo número: ");
+				
+		//Comando para introducir el primer número
+		b = sc.nextDouble();
+		
+		//Le decimos al usuario que introduzca un número
+		System.out.print("Introduce el tercer número: ");
+						
+		//Comando para introducir el primer número
+		c = sc.nextDouble();
+		
+		raiz = Math.sqrt((b*b) - 4*a*c);
+		xsum=(-b + raiz)/2*a;
+		xres=(-b - raiz)/2*a;
+		
+		System.out.print("Los valores de X son: " + xsum + xres);
+		
 	}
 
 }
